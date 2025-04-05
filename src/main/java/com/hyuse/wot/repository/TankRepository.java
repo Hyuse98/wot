a295f4390119dac9e479d8ae4e6e65cc07cc4400
Franklin <franklinpereira98@hotmail.com>
2025-04-05 18:19:31 -0300

    Adding Repository Interface

---
+++ src/main/java/com/hyuse/wot/repository/TankRepository.java
@@ -0,0 +1,13 @@
+package com.hyuse.wot.repository;
+
+import com.hyuse.wot.model.Tank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TankRepository extends JpaRepository<Tank, Long> {
+
+    Optional<Tank> findTankByName(String name);
+}
