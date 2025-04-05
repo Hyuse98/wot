21a48ebccc1330d5fb57dbf6490c6795db06b642
Franklin <franklinpereira98@hotmail.com>
2025-04-05 10:53:55 -0300

    Initial commit

---
+++ src/main/java/com/hyuse/wot/WotApplication.java
@@ -0,0 +1,13 @@
+package com.hyuse.wot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyuse/wot/WotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuse.wot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
